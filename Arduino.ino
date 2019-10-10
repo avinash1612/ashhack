e7875c135bcfbee2e2380e5d2f1732c811832275
avinash1612 <56393465+avinash1612@users.noreply.github.com>
2019-10-10 17:10:58 +0530

    Update Arduino.ino

--- Arduino.ino
+++ Arduino.ino
@@ -1,10 +1,11 @@
 int timer = 100;          
 int ledPins[] = {
-  2, 7, 4, 6, 5, 3
+  2, 7, 4, 6, 5, 8,9
 };       
-int pinCount = 6;           
+int pinCount = 7;           
 
 void setup() {
+  Serial.begin(96500);
   for (int thisPin = 0; thisPin < pinCount; thisPin++) {
     pinMode(ledPins[thisPin], OUTPUT);
   }
@@ -16,16 +17,19 @@ void loop() {
    
     digitalWrite(ledPins[thisPin], HIGH);
     delay(timer);
+    delay(3000);
     
     digitalWrite(ledPins[thisPin], LOW);
+    Serial.print("Timer starts");
 
   }
 
   
-  for (int thisPin = pinCount - 1; thisPin >= 0; thisPin--) {
+  for (int thisPin = pinCount - 1; thisPin >= 0; thisPin--) {Serial.print("Timer stops");
     
     digitalWrite(ledPins[thisPin], HIGH);
     delay(timer);
+    delay(1000);
     digitalWrite(ledPins[thisPin], LOW);
   }
 }
